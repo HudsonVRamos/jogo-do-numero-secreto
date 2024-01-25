ca4fba9550fe2c83c44f5932a0a7d33ce369e5e8
HudsonVRamos <139950979+HudsonVRamos@users.noreply.github.com>
2024-01-25 11:22:34 -0300

    Add files via upload

---
+++ js-curso-2-aula_-/desafio2.js
@@ -0,0 +1,41 @@
+/*function OlaMundo() {
+    console.log('Olá mundo!');
+}
+OlaMundo();*/
+
+/*let nome = prompt('Digite seu nome');
+function OlaNome() {
+    console.log(`Olá ${nome}`);
+}
+OlaNome();*/
+
+/*function dobroDoNumero(numero) {
+    return numero * 2;
+}
+let resultadodoDobro = dobroDoNumero(5);
+console.log(resultadodoDobro);*/
+
+/*function mediaDosNumeros(numero1, numero2, numero3) {
+    return ((numero1 + numero2 + numero3) / 3)
+}
+let media = mediaDosNumeros(1, 2, 3);
+console.log(media);*/
+
+/*function qualNumeroMaior(a, b) {
+    return a > b ? a : b;
+}
+let maiorNumero = qualNumeroMaior(5, 3);
+console.log(maiorNumero);*/
+
+/*function multiplicacaoPorEleMesmo(numero) {
+    return multiplicacao = numero * numero
+
+}
+let resultado = multiplicacaoPorEleMesmo(2);
+console.log(resultado);*/
+
+
+
+
+
+
---
+++ js-curso-2-aula_-/desafio4.js
@@ -0,0 +1,9 @@
+let listaGenerica = [];
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+
+let = listaTresNomes = ['Hudson', 'Natalia', 'Arthur'];
+console.log(listaTresNomes[0]);
+console.log(listaTresNomes[1]);
+console.log(listaTresNomes[2]);
